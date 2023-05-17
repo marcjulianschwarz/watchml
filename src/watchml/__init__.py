ee276e5d2c98b3d11b4902889b2e46a237a7460c
marcjulianschwarz <info@marc-julian.de>
2023-05-17 16:05:42 +0200

    add init file imports

--- src/watchml/__init__.py
+++ src/watchml/__init__.py
@@ -1,12 +1,16 @@
 import logging
 import sys
 
+from . import data
+from . import file
+from . import ml
+from . import utils
+from . import viz
 from .data import *
 from .file import *
 from .ml import *
 from .utils import *
 from .viz import *
-
 
 logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
 logging.info("Imported watchml")
